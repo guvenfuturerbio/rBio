83b8c8b9d72aa474b285dbd82b6c01004b3d6562
birhos <haydarrdemir@hotmail.com>
2021-12-15 14:08:06 +0300

    PatientList düzenlendi.

--- lib/features/doctor/treatment_process/view/treatment_process_screen.dart
+++ lib/features/doctor/treatment_process/view/treatment_process_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../core/core.dart';
+
+part '../model/treatment_process_model.dart';
+part '../viewmodel/treatment_process_vm.dart';
 
 class DoctorTreatmentProcessScreen extends StatefulWidget {
   DoctorTreatmentProcessScreen({Key key}) : super(key: key);
@@ -17,7 +21,15 @@ class _DoctorTreatmentProcessScreenState
   Widget build(BuildContext context) {
     return RbioScaffold(
       appbar: _buildAppBar(),
-      body: _buildBody(),
+      body: Consumer<TreatmentProcessVm>(
+        builder: (
+          BuildContext context,
+          TreatmentProcessVm vm,
+          Widget child,
+        ) {
+          return _buildBody(vm);
+        },
+      ),
     );
   }
 
@@ -39,86 +51,107 @@ class _DoctorTreatmentProcessScreenState
         ],
       );
 
-  Widget _buildBody() {
+  Widget _buildBody(TreatmentProcessVm vm) {
+    switch (vm.progress) {
+      case LoadingProgress.LOADING:
+        return RbioLoading();
+
+      case LoadingProgress.DONE:
+        return _buildSuccess(vm);
+
+      case LoadingProgress.ERROR:
+        return RbioBodyError();
+
+      default:
+        return SizedBox();
+    }
+  }
+
+  Widget _buildSuccess(TreatmentProcessVm vm) {
     return ListView.builder(
       padding: EdgeInsets.zero,
       scrollDirection: Axis.vertical,
       physics: BouncingScrollPhysics(),
-      itemCount: 10,
+      itemCount: vm.list.length,
       itemBuilder: (BuildContext context, int index) {
-        return _buildCard();
+        return _buildCard(vm.list[index]);
       },
     );
   }
 
-  Widget _buildCard() {
-    return Card(
-      elevation: 0,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 8,
-          horizontal: 12,
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            //
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  //
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      //
-                      Expanded(
-                        child: Text(
-                          'Görüntülü Görüşme',
+  Widget _buildCard(TreatmentProcessItemModel item) {
+    return GestureDetector(
+      onTap: () {
+        Atom.to(PagePaths.DOCTOR_VIDEO_CALL_EDIT);
+      },
+      child: Card(
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 12,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              //
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    //
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        //
+                        Expanded(
+                          child: Text(
+                            item.title ?? '',
+                            style: context.xHeadline4.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+
+                        //
+                        Text(
+                          item.dateTime ?? '',
                           style: context.xHeadline4.copyWith(
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                      ),
-
-                      //
-                      Text(
-                        '01/01/2021 - 09:00',
-                        style: context.xHeadline4.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  //
-                  Text(
-                    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh',
-                    style: context.xHeadline5.copyWith(
-                      color: getIt<ITheme>().textColorPassive,
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ),
 
-            //
-            Padding(
-              padding: EdgeInsets.only(
-                left: 10,
+                    //
+                    Text(
+                      item.description ?? '',
+                      style: context.xHeadline5.copyWith(
+                        color: getIt<ITheme>().textColorPassive,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              child: SvgPicture.asset(
-                R.image.arrow_right_icon,
-                height: 15,
+
+              //
+              Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                ),
+                child: SvgPicture.asset(
+                  R.image.arrow_right_icon,
+                  height: 15,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
