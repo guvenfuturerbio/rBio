d55335cb0ca560f473449723900f17380ed77eb9
Mustafa TÜRKMEN <mustafa.turkmen@guvenfuture.com>
2021-12-28 18:46:50 +0300

    Patch for auto login

--- lib/core/manager/user_manager.dart
+++ lib/core/manager/user_manager.dart
@@ -74,12 +74,12 @@ class UserManagerImpl extends UserManager {
     UserLoginInfo userLoginInfo = UserLoginInfo();
     String username = getIt<ISharedPreferencesManager>()
         .getString(SharedPreferencesKeys.LOGIN_USERNAME);
-    // userLoginInfo.username = username;
-    userLoginInfo.username = '18620716416';
+    userLoginInfo.username = username;
+    // userLoginInfo.username = '18620716416';
     String password = getIt<ISharedPreferencesManager>()
         .getString(SharedPreferencesKeys.LOGIN_PASSWORD);
-    // userLoginInfo.password = password;
-    userLoginInfo.password = 'Numlock1234!!';
+    userLoginInfo.password = password;
+    // userLoginInfo.password = 'Numlock1234!!';
     return userLoginInfo;
   }
 
