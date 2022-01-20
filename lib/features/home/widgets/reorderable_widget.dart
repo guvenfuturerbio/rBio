d5e892028f4fcfe0484c0eb470c5ec56ffe591d3
birhos <haydarrdemir@hotmail.com>
2022-01-20 21:28:00 +0300

    Home localization fix

--- lib/features/home/widgets/reorderable_widget.dart
+++ lib/features/home/widgets/reorderable_widget.dart
@@ -29,90 +29,84 @@ class MyReorderableWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return RbioOrientationBuilder(
       builder: (BuildContext context, AsyncSnapshot<Orientation> snapshot) {
-        return Consumer<LocaleNotifier>(
-          builder: (context, vm, child) {
-            if (snapshot.hasData) {
-              return Consumer<HomeVm>(
-                builder: (BuildContext context, HomeVm vm, Widget child) {
-                  final Widget child = ReorderableWidget(
-                    key: key,
-                    reorderable: vm.status.isShaken,
-                    child: Stack(
-                      alignment: Alignment.topRight,
-                      children: [
-                        //
-                        Spring.rotate(
-                          springController: vm.springController,
-                          alignment: Alignment.center, //def=center
-                          startAngle: vm.startAngle, //def=0
-                          endAngle: vm.endAngle, //def=360
-                          animDuration:
-                              const Duration(milliseconds: 150), //def=1s
-                          animStatus: (AnimStatus status) {},
-                          curve: Curves.linear, //def=Curves.easInOut
-                          child: !isVerticalCard
-                              ? body
-                              : GestureDetector(
-                                  onTap: () {
-                                    if (vm.showDeletedAlert) {
-                                      vm.addWidget(type);
-                                    } else if (vm.status ==
-                                        ShakeMod.notShaken) {
-                                      if (onTap != null) {
-                                        onTap();
-                                      }
-                                    }
-                                  },
-                                  child: body,
-                                ),
-                        ),
-
-                        //
-                        if (showDeleteIcon) ...[
-                          Visibility(
-                            visible: vm.status.isShaken,
-                            child: GestureDetector(
+        if (snapshot.hasData) {
+          return Consumer<HomeVm>(
+            builder: (BuildContext context, HomeVm vm, Widget child) {
+              final Widget child = ReorderableWidget(
+                key: key,
+                reorderable: vm.status.isShaken,
+                child: Stack(
+                  alignment: Alignment.topRight,
+                  children: [
+                    //
+                    Spring.rotate(
+                      springController: vm.springController,
+                      alignment: Alignment.center, //def=center
+                      startAngle: vm.startAngle, //def=0
+                      endAngle: vm.endAngle, //def=360
+                      animDuration: const Duration(milliseconds: 150), //def=1s
+                      animStatus: (AnimStatus status) {},
+                      curve: Curves.linear, //def=Curves.easInOut
+                      child: !isVerticalCard
+                          ? body
+                          : GestureDetector(
                               onTap: () {
-                                vm.removeWidget(type);
+                                if (vm.showDeletedAlert) {
+                                  vm.addWidget(type);
+                                } else if (vm.status == ShakeMod.notShaken) {
+                                  if (onTap != null) {
+                                    onTap();
+                                  }
+                                }
                               },
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.red.withOpacity(.6),
-                                ),
-                                height: 30,
-                                width: 30,
-                                child: const Icon(
-                                  Icons.remove,
-                                  color: Colors.white,
-                                  size: 10,
-                                ),
-                              ),
+                              child: body,
+                            ),
+                    ),
+
+                    //
+                    if (showDeleteIcon) ...[
+                      Visibility(
+                        visible: vm.status.isShaken,
+                        child: GestureDetector(
+                          onTap: () {
+                            vm.removeWidget(type);
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.red.withOpacity(.6),
+                            ),
+                            height: 30,
+                            width: 30,
+                            child: const Icon(
+                              Icons.remove,
+                              color: Colors.white,
+                              size: 10,
                             ),
                           ),
-                        ],
-                      ],
-                    ),
-                  );
-
-                  if (!vm.status.isShaken && !vm.showDeletedAlert) {
-                    return InkWell(
-                      onLongPress: () {
-                        vm.changeStatus();
-                      },
-                      onTap: () {},
-                      child: child,
-                    );
-                  } else {
-                    return child;
-                  }
-                },
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
               );
-            }
 
-            return SizedBox();
-          },
-        );
+              if (!vm.status.isShaken && !vm.showDeletedAlert) {
+                return InkWell(
+                  onLongPress: () {
+                    vm.changeStatus();
+                  },
+                  onTap: () {},
+                  child: child,
+                );
+              } else {
+                return child;
+              }
+            },
+          );
+        }
+
+        return SizedBox();
       },
     );
   }
