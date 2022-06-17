5b2b2de577b0c4ccd76879656e25451a2b8b136d
birhos <haydarrdemir@hotmail.com>
2022-06-17 14:50:56 +0300

    Onedose Sentry added

--- lib/core/manager/sentry_manager.dart
+++ lib/core/manager/sentry_manager.dart
@@ -63,13 +63,36 @@ class GuvenOnlineSentryManagerImpl extends SentryManager {
 }
 
 class OneDoseSentryManagerImpl extends SentryManager {
+  final String _dsn =
+      'https://cdaf2769f32349d8baf6fa2f97d04aa5@o983734.ingest.sentry.io/6511487';
+  final String _release = 'com.guvenfuture.onedosehealth@';
+
   @override
-  Future<void> init(FutureOr<void> Function() appRunner, String version) async {
-    await appRunner();
+  Future<void> init(
+    FutureOr<void> Function() appRunner,
+    String version,
+  ) async {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = _dsn;
+        options.enablePrintBreadcrumbs = true;
+        options.attachStacktrace = true;
+        options.release = _release + version;
+        options.environment = kDebugMode
+            ? Environment.dev.toString()
+            : Environment.prod.toString();
+        options.diagnosticLevel = SentryLevel.debug;
+        options.sendDefaultPii = true;
+      },
+      appRunner: appRunner,
+    );
   }
 
   @override
   Widget wrapBundle(Widget child) {
-    return child;
+    return DefaultAssetBundle(
+      bundle: SentryAssetBundle(enableStructuredDataTracing: true),
+      child: child,
+    );
   }
 }
