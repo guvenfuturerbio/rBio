30dd23f0ba7d00994e62b5fa7f83e2b625b54ca5
birhos <haydarrdemir@hotmail.com>
2022-06-29 15:41:34 +0300

    ApiService added

--- packages/key_manager/lib/src/key_manager.dart
+++ packages/key_manager/lib/src/key_manager.dart
@@ -3,10 +3,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../key_manager.dart';
 
 class KeyManager {
-  final _map = <Keys, String>{};
+  final map = <Keys, String>{};
 
   String get(Keys key) {
-    final value = _map[key];
+    final value = map[key];
     if (value == null) {
       throw Exception("$key valu null");
     }
@@ -21,7 +21,7 @@ class KeyManager {
     await dotenv.load(fileName: filePath);
     final local =
         dotenv.env.map((key, value) => MapEntry(_fromString(key), value));
-    _map.addAll({...local, ...remote});
+    map.addAll({...local, ...remote});
   }
 
   Keys _fromString(String s) => Keys.values.firstWhere((v) => v.name == s);
