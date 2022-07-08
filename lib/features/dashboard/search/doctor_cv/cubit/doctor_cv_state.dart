c68c2d86c82b605b7cd303c11fca45f7355dba80
Haydar DEMİR <haydar.demir@guvenfuture.com>
2022-07-08 06:34:19 +0000

    Merged PR 244: doctor cv bloc revize edildi.
    doctor cv bloc revize edildi.
    
    Cherry picked from !242
    
    Cherry-picked from commit `5033b4ae`.
    
    Cherry picked from !243
    
    Cherry-picked from commit `f4f17c42`.

--- lib/features/dashboard/search/doctor_cv/cubit/doctor_cv_state.dart
+++ lib/features/dashboard/search/doctor_cv/cubit/doctor_cv_state.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'doctor_cv_cubit.dart';
 
 @freezed
 class DoctorCvState with _$DoctorCvState {
   const factory DoctorCvState.initial() = _InitialState;
-  const factory DoctorCvState.success(DoctorCvResponse result) = _SuccessState;
+  const factory DoctorCvState.success(DoctorCvResponse result,String imageUrl) = _SuccessState;
   const factory DoctorCvState.loading() = _LoadingState;
   const factory DoctorCvState.error(DoctorCvResponse? result) = _ErrorState;
 }
+
+
